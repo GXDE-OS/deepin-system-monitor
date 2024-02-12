ff4c69b2301d61baae284cefc8976921b1270a78
gfdgd-xi <3025613752@qq.com>
2024-02-12 01:18:20 +0000

    6.0.8

---
+++ deepin-system-monitor-main/main.cpp
@@ -0,0 +1,111 @@
+// Copyright (C) 2011 ~ 2020 Uniontech Software Technology Co.,Ltd
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "environments.h"
+#include "accessible.h"
+#include "common/common.h"
+#include "application.h"
+#include "settings.h"
+#include "gui/main_window.h"
+#include "common/perf.h"
+#include "dbus/dbus_object.h"
+#include "dbus/dbusalarmnotify.h"
+#include "3rdparty/dmidecode/dmidecode.h"
+
+#include <DApplication>
+#include <DApplicationSettings>
+#include <DGuiApplicationHelper>
+#include <DMainWindow>
+#include <DWidgetUtil>
+#include <DLog>
+
+#include <QApplication>
+#include <QDateTime>
+#include <QAccessible>
+
+DWIDGET_USE_NAMESPACE
+DCORE_USE_NAMESPACE
+
+using namespace common::init;
+
+int main(int argc, char *argv[])
+{
+    //Judge if Wayland
+    WaylandSearchCentered();
+    if (!QString(qgetenv("XDG_CURRENT_DESKTOP")).toLower().startsWith("deepin")) {
+        setenv("XDG_CURRENT_DESKTOP", "Deepin", 1);
+    }
+
+    Application::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
+    Application app(argc, argv);
+    QCommandLineParser parser;
+    parser.process(app);
+    QStringList allArguments = parser.positionalArguments();
+    if (allArguments.size() == 3 && allArguments.first().compare("alarm", Qt::CaseInsensitive) == 0) {
+        app.loadTranslator();
+        DBusAlarmNotify::getInstance().showAlarmNotify(allArguments);
+        return 0;
+    }
+
+    //=======通知已经打开的进程
+    if (!DBusObject::getInstance().registerOrNotify())
+        return 0;
+
+    //获取dmidecode中CPU频率信息
+    char *const cmd[] = {"dmidecode", "-t", "4"};
+    get_cpuinfo_from_dmi(3, cmd);
+    PERF_PRINT_BEGIN("POINT-01", "");
+
+    app.setAutoActivateWindows(true);
+    //设置单例
+    DGuiApplicationHelper::setSingleInstanceInterval(-1);
+    if (DGuiApplicationHelper::setSingleInstance(QString("deepin-system-monitor"),
+                                                 DGuiApplicationHelper::UserScope)) {
+        app.loadTranslator();
+
+        const QString descriptionText = DApplication::translate(
+                                            "App.About",
+                                            "System Monitor is a tool to monitor realtime system load, "
+                                            "view and control processes and services running on your system.");
+
+        const QString acknowledgementLink =
+            "https://www.deepin.org/acknowledgments/deepin-system-monitor#thanks";
+
+        app.setOrganizationName("deepin");
+        app.setApplicationName("deepin-system-monitor");
+        app.setApplicationDisplayName(DApplication::translate("App.About", "System Monitor"));
+        app.setApplicationVersion(VERSION);
+
+        app.setProductIcon(QIcon::fromTheme("deepin-system-monitor"));
+        app.setProductName(DApplication::translate("App.About", "System Monitor"));
+        app.setApplicationDescription(descriptionText);
+        app.setApplicationAcknowledgementPage(acknowledgementLink);
+
+        //app.setWindowIcon(QIcon::fromTheme("deepin-system-monitor")); //耗时40ms
+
+        QAccessible::installFactory(accessibleFactory);
+
+        DApplicationSettings appSettings;
+
+        DLogManager::registerConsoleAppender();
+        DLogManager::registerFileAppender();
+
+        MainWindow mw;
+        gApp->setMainWindow(&mw);
+        mw.initDisplay();
+
+        QObject::connect(&app,
+                         &DApplication::newInstanceStarted,
+                         &mw,
+                         &MainWindow::activateWindow);
+
+        Dtk::Widget::moveToCenter(&mw);
+        mw.show();
+
+        return app.exec();
+    }
+
+    return 0;
+}
---
+++ deepin-system-monitor-plugin-popup/main.cpp
@@ -0,0 +1,68 @@
+// Copyright (C) 2019 ~ 2020 Uniontech Software Technology Co.,Ltd.
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "gui/mainwindow.h"
+#include "dde-dock/constants.h"
+#include "application.h"
+
+//#include "clipboard_adaptor.h"
+
+#include <DApplication>
+#include <DGuiApplicationHelper>
+#include <DLog>
+#include <unistd.h>
+#include <QDBusInterface>
+
+
+
+DGUI_USE_NAMESPACE
+DWIDGET_USE_NAMESPACE
+DCORE_USE_NAMESPACE
+
+int main(int argc, char *argv[])
+{
+    // 前置wayland环境变量
+    if (!qgetenv("WAYLAND_DISPLAY").isEmpty()) {
+        qputenv("QT_QPA_PLATFORM", "wayland");
+        qputenv("QT_WAYLAND_SHELL_INTEGRATION", "kwayland-shell");
+    }
+
+    DGuiApplicationHelper::setUseInactiveColorGroup(false);
+    DGuiApplicationHelper::setColorCompositingEnabled(true);
+
+//    DApplication *app = DApplication::globalApplication(argc, argv);
+
+    Application::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
+    Application ac(argc, argv);
+    ac.setAutoActivateWindows(true);
+
+    ac.setOrganizationName("deepin");
+    ac.setApplicationName("deepin-system-monitor-plugin-popup");
+    ac.setApplicationDisplayName("deepin-system-monitor-plugin-popup");
+    ac.setApplicationVersion("1.0");
+
+
+    if (!DGuiApplicationHelper::setSingleInstance(QString("deepin-system-monitor-plugin-popup"))) {
+        qDebug() << "set single instance failed!";
+        return -1;
+    }
+
+    ac.loadTranslator();
+
+    MainWindow w;
+    gApp->setMainWindow(&w);
+
+    QDBusInterface interface("com.deepin.SystemMonitorPluginPopup", "/com/deepin/SystemMonitorPluginPopup",
+                                 "com.deepin.SystemMonitorPluginPopup",
+                                 QDBusConnection::sessionBus());
+    if (!interface.isValid()) {
+        qDebug() << "start loader...";
+        w.startLoader();
+    }
+#ifdef QT_DEBUG
+    w.showAni();
+#endif
+    return ac.exec();
+}
